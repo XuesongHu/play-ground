284ccb3dda737c99380aee274fdf9493771a9954
Xuesong Hu <xuesong.hu@ibm.com>
2019-10-05 13:51:49 -0400

    Adding an assertion to test business logic.

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -20,9 +20,15 @@ func TestHelloServer(t *testing.T) {
 	g := Greeter{}
 	ctx := context.Background()
 	req := new(proto.HelloRequest)
+	req.Name = "John"
 	rsp := new(proto.HelloResponse)
 	got := g.Hello(ctx, req, rsp)
 	if got != nil {
 		t.Errorf("got %q and want nil", got)
+	}
+	res := rsp.GetGreeting()
+	want := "Hello John"
+	if res != want {
+		t.Errorf("got %q and want %q", res, want)
 	}
 }
